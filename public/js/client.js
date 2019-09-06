67547cdf70850a49d6e8336cd6ab3db1b858485e
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-09-06 15:49:58 -0300

    setting up the page to show collected api data

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,26 @@
-console.log("Client side javascript file is loaded.");
+const log = console.log;
+
+const weatherForm = document.querySelector("div.main-content form");
+const search = document.querySelector("div.main-content form input");
+const errorLocation = document.querySelector("div.main-content p#error-loc");
+const forecast = document.querySelector("div.main-content p#forecast");
+
+weatherForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const location = search.value;
+
+  errorLocation.textContent = "Loading...";
+  forecast.textContent = "";
+
+  fetch(`/weather?address=${location}`).then(response => {
+    response.json().then(parsedData => {
+      if (parsedData.error) {
+        errorLocation.textContent = parsedData.error;
+      } else {
+        errorLocation.textContent = parsedData.location;
+        forecast.textContent = parsedData.forecast;
+      }
+    });
+  });
+});
